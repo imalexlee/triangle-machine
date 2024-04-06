7398db9d306a0bb869ba100b757de99eecdd8996
imalexlee <alexdlee1217@gmail.com>
2024-04-06 00:27:31 -0400

    descriptor set operations

--- src/vk_backend/resources/vk_descriptor.h
+++ src/vk_backend/resources/vk_descriptor.h
@@ -1,14 +1,52 @@
 #pragma once
 
-#include "vk_backend/vk_device.h"
+#include <cstdint>
 #include <span>
+#include <vector>
 #include <vk_backend/vk_types.h>
+#include <vulkan/vulkan_core.h>
 
-class DescriptorContext {
+struct PoolSizeRatio {
+  VkDescriptorType type;
+  uint32_t desc_per_set;
+};
+
+class DescriptorLayoutBuilder {
 public:
-  VkDescriptorPool pool;
-
-  void create(DeviceContext& device_context, std::span<VkDescriptorPoolSize> pool_sizes);
+  void add_binding(uint32_t binding, VkDescriptorType type);
+  void clear();
+  VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shader_stages);
 
 private:
+  std::vector<VkDescriptorSetLayoutBinding> _bindings;
+};
+
+class DescriptorWriter {
+public:
+  void write_image(int binding, VkImageView image, VkSampler sampler, VkImageLayout layout, VkDescriptorType type);
+  void write_buffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type);
+  void clear();
+  void update_set(VkDevice device, VkDescriptorSet set);
+
+private:
+  std::deque<VkDescriptorImageInfo> _image_infos;
+  std::deque<VkDescriptorBufferInfo> _buffer_infos;
+  std::vector<VkWriteDescriptorSet> _writes;
+};
+
+class DescriptorAllocator {
+public:
+  void create(VkDevice device, uint32_t max_sets, std::span<PoolSizeRatio> pool_size_ratios);
+  VkDescriptorSet allocate(VkDevice device, VkDescriptorSetLayout layout);
+  void clear_pools(VkDevice device);
+  void destroy_pools(VkDevice device);
+
+private:
+  std::vector<VkDescriptorPool> _ready_pools;
+  std::vector<VkDescriptorPool> _full_pools;
+  std::vector<PoolSizeRatio> _ratios;
+  uint32_t _sets_per_pool;
+
+  VkDescriptorPool create_pool(VkDevice device, uint32_t max_sets, std::span<PoolSizeRatio> pool_size_ratios);
+  VkDescriptorPool get_pool(VkDevice device);
 };
